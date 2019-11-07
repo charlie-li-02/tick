f33d64835c29f2383d98c1227f5e50f8448976de
Charlie <charlie.zh.li.1025@gmail.com>
2019-11-06 21:45:41 -0800

    deliverable 10 v2

--- src/main/network/ReadWebPageWeather.java
+++ src/main/network/ReadWebPageWeather.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.text.DecimalFormat;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
